718bf95b3be67a89e8c97e26b11c23893100cb8b
Victor Plushko <vicplushko33@gmail.com>
2020-04-27 06:17:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,4 +13,4 @@ end
 #image link :project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text
-#percent funded: project.css("ul.project-stats li.first.funded").text
+#percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
